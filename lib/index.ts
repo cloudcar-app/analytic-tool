e3b2bfe2920d1746a5c13e4ba4b60d2b340161da
Vicente Vega Ulloa <vnvega@uc.cl>
2022-01-13 15:40:23 -0300

    refactor: change config path

--- lib/index.ts
+++ lib/index.ts
@@ -9,8 +9,8 @@ import { DebuggerPlugin } from '@snowplow/browser-plugin-debugger';
 import * as path from 'path';
 
 console.log(path.dirname(require.main.filename));
-// const trackersConfig = require(path.join('./../../../..', 'snowplow-config.json'));
-// console.log(path.join(process.cwd(), 'snowplow-config.json'))
+const trackersConfig = require(path.join(path.dirname(require.main.filename), 'snowplow-config.json'));
+console.log(trackersConfig)
 // console.log(path.join('./../../../..', 'snowplow-config.json'));
 
 // import trackPagePingExtended from './custom_tracker/page_ping_extended';
@@ -25,12 +25,12 @@ function snowplowService(collectorAddress: string): void {
         webPage: true,
         },
     });
-    // if (trackersConfig.enableActivityTracking) {
-    //     enableActivityTracking(trackersConfig.enableActivityTracking);
-    // }
-    // if (trackersConfig.trackPageView) {
-    //     trackPageView();
-    // }
+    if (trackersConfig.enableActivityTracking) {
+        enableActivityTracking(trackersConfig.enableActivityTracking);
+    }
+    if (trackersConfig.trackPageView) {
+        trackPageView();
+    }
 }
 
 export default snowplowService;
