6066a0ffbc9857382a0d565d4688532742be8714
Vicente Vega Ulloa <vnvega@uc.cl>
2022-01-13 16:21:22 -0300

    refactor: change config path

--- lib/index.ts
+++ lib/index.ts
@@ -7,11 +7,15 @@ import { DebuggerPlugin } from '@snowplow/browser-plugin-debugger';
 // import trackersConfig from './../../../../snowplow-config.json';
 
 import * as path from 'path';
+import * as process from 'process';
+
+var appRoot = require('app-root-path');
+var myModule = require(appRoot + '/snowplow-config.json');
 
 console.log(path.dirname(require.main.filename));
 const trackersConfig = require(path.join(path.dirname(require.main.filename), 'snowplow-config.json'));
-console.log(trackersConfig)
-// console.log(path.join('./../../../..', 'snowplow-config.json'));
+console.log(trackersConfig);
+console.log(path.join(path.dirname(require.main.filename), 'snowplow-config.json'));
 
 // import trackPagePingExtended from './custom_tracker/page_ping_extended';
 
