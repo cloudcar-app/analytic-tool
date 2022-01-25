46094fbb841649d2c239dd76bd345d57cb069445
mp11b <maponce@miuandes.cl>
2022-01-25 16:09:04 -0300

    refactor: Add trackPagePingExtended to tracker index

--- lib/custom_trackers/index.ts
+++ lib/custom_trackers/index.ts
@@ -2,10 +2,12 @@ import trackTextSelection from './text_selection';
 import trackParticularClicks from './particular_clicks';
 import trackPurchaseButtonClick from './purchase_button_click';
 import trackHover from './hover';
+import trackPagePingExtended from './page_ping_extended';
 
 export {
     trackTextSelection,
     trackParticularClicks,
     trackPurchaseButtonClick,
-    trackHover
+    trackHover,
+    trackPagePingExtended
 };
