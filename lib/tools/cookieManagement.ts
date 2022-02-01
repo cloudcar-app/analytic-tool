8928513d5d0190d5f4cd4d80a64b8686d74f51f5
mp11b <maponce@miuandes.cl>
2022-02-01 13:10:08 -0300

    feat: Add cookie management file

---
+++ lib/tools/cookieManagement.ts
@@ -0,0 +1,35 @@
+const createCookie = (key:string, value:string, expirationDay:number)=> {
+    var date:Date = new Date(); 
+    date.setTime(date.getTime() + (expirationDay*24*60*60*1000)); 
+    var expires:string = "expires="+date.toUTCString(); 
+    document.cookie = `${key}=${value};secure;${expires}`; 
+}
+
+const updateCookie = (key:string, value:string) =>{
+    document.cookie = `${key}=${value};secure`;
+}
+
+const checkCookie = (cookieName:string) => {
+    var key:string = getCookieByName(cookieName);
+    if (key!=""){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const getCookieByName = (cookieName: string) => {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${cookieName}=`);
+    if (parts.length === 2){
+        return parts.pop().split(';').shift();
+    }
+}
+
+export {
+    createCookie,
+    updateCookie,
+    checkCookie,
+    getCookieByName
+};
