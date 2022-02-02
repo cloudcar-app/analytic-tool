44819362954babf204fb33e1178cac2b56d773bc
Vicente Vega Ulloa <vnvega@uc.cl>
2022-02-02 17:46:34 -0300

    feat: Add dynamic config support

--- lib/tools/getUnseenElements.ts
+++ lib/tools/getUnseenElements.ts
@@ -1,27 +1,39 @@
-import { 
+import {
     TrackedElement,
     Selector,
-  } from '../config/configTypes';
+} from '../config/configTypes';
 
+const setIdToTrackedElement = (selectorId: string, element: HTMLElement): string => {
+    const substrings: string[] = selectorId.split('#') 
+    if (substrings.length < 3) { return selectorId; }
+    for (let index = 1; index < substrings.length; index += 2) {
+        const substring = substrings[index];
+        substrings[index] = element.getAttribute(substring) || 'null';
+    }
+    return substrings.join('')
+}
 
 /**
  * Este método busca elementos en base a `selectors` 
  * y retorna los elementos que encuentra y que no
  * están en `relevantElements`
  */
-export const getUnseenElements = (selectors: (Selector)[], relevantElements: (TrackedElement)[]): Array<TrackedElement> => {
+export const getUnseenElements = (
+    selectors: (Selector)[], 
+    relevantElements: (TrackedElement)[]
+    ): Array<TrackedElement> => {
     let newElements: Array<TrackedElement> = [];
     selectors.forEach((selector: Selector) => {
         let queryElements: Array<HTMLElement> = Array.from(window.document.querySelectorAll(selector.css_selector))
         newElements.push(
-        ...queryElements.map((element: HTMLElement) => ({
-            id: selector.selector_id,
-            step: (selector.step) ? selector.step : '',
-            element: element,
-        }))
-        ) 
+            ...queryElements.map((element: HTMLElement) => ({
+                id: setIdToTrackedElement(selector.selector_id, element),
+                step: (selector.step) ? selector.step : '',
+                element: element,
+            }))
+        )
     })
     return newElements
         .filter((newElement: TrackedElement) => !relevantElements
-        .some((relevantElement: TrackedElement) => relevantElement.element == newElement.element));
+            .some((relevantElement: TrackedElement) => relevantElement.element == newElement.element));
 }
